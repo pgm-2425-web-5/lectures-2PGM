9c0cc61485ba66ebf0c022e7123f5953307d8f24
Michael Vanderpoorten <michael@yungpanda.com>
2024-09-24 10:17:15 +0200

    Les 1

---
+++ week-1/react-basis/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Title from "./Title";
+
+const App = () => {
+  return (
+    <>
+      <Title showTitle={true}>Hallo iedereen</Title>
+      <p>Ik ben React</p>
+    </>
+  );
+};
+
+export default App;
